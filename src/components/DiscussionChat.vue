45f35ee2fbb210716f8f3731f79a5b148ace65a3
Julia Arruda <julia.arruda@ifood.com.br>
2020-09-06 10:12:39 -0300

    not comment on inactive

--- src/components/DiscussionChat.vue
+++ src/components/DiscussionChat.vue
@@ -4,15 +4,39 @@
             <v-row>
                 <v-col class="col-12">
                     <v-list dense nav class="py-0">
-                    <v-list-item two-line>
-                        <v-list-item-avatar>
-                        <img :src="discussion.photo">
-                        </v-list-item-avatar>
-                        <span class="name">{{ discussion.name }}</span>
-                    </v-list-item>
-                    <v-list-item>
-                        <p class="description">{{discussion.description}}</p>
-                    </v-list-item>
+                        <v-list-item two-line>
+                            <v-list-item-avatar>
+                                <img :src="discussion.photo">
+                            </v-list-item-avatar>
+                            <span class="name">{{ discussion.name }}</span>
+                        </v-list-item>
+                        <v-list-item>
+                            <v-col>
+                                <p class="description">{{discussion.description}}</p>
+                            </v-col>
+                            <v-col v-if="!isActive">
+                                <v-chip
+                                    class="ma-1"
+                                    color="#2988B1"
+                                    text-color="white"
+                                >
+                                    <v-avatar left>
+                                        <v-icon small>mdi-thumb-up</v-icon>
+                                    </v-avatar>
+                                    {{discussion.likes}}
+                                </v-chip>
+                                <v-chip
+                                    class="ma-1"
+                                    color="#2988B1"
+                                    text-color="white"
+                                >
+                                    <v-avatar left>
+                                        <v-icon small>mdi-thumb-down</v-icon>
+                                    </v-avatar>
+                                    {{discussion.dislikes}}
+                                </v-chip>
+                            </v-col>
+                        </v-list-item>
                     </v-list>
                 </v-col>
             </v-row>
@@ -30,6 +54,7 @@
                 </v-row>
             </v-container>
             <v-text-field
+                v-if="isActive"
                 label="Message"
                 outlined
                 v-model="comment.text"
@@ -38,8 +63,7 @@
         <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn class="confirmation-button" text @click="exit">Sair</v-btn>
-            <v-btn class="confirmation-button" text @click="cancel">Cancelar</v-btn>
-            <v-btn class="confirmation-button" text @click="send">Enviar</v-btn>
+            <v-btn class="confirmation-button" v-if="isActive" text @click="send">Enviar</v-btn>
         </v-card-actions>
     </v-card>
 </template>
@@ -64,9 +88,6 @@ export default {
       send() {
         this.discussion.comments.push(JSON.parse(JSON.stringify(this.comment)))
         this.comment.text = ''
-      },
-      cancel () {
-            this.comment.text = ''
       }
   }
 }
