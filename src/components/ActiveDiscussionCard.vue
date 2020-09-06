3684a23d91e43ca06cce2598aba75d14f539bae9
Julia Arruda <julia.arruda@ifood.com.br>
2020-09-05 22:51:08 -0300

    start inactive

--- src/components/ActiveDiscussionCard.vue
+++ src/components/ActiveDiscussionCard.vue
@@ -1,12 +1,32 @@
 <template>
-  <v-card>
-    
+  <v-card class="card">
+        <v-card-text class="card-text">
+            <div>
+                <span class="discussion"> Em discussão </span>
+            </div>
+            <div class="main-class">
+                <div class="first-grid">
+                    <img :src="discussion.photo" class="photo">
+                    <v-icon class="comment-icon">mdi-comment</v-icon>
+                </div>
+                <div>
+                    <span class="name">{{ discussion.name }}</span>
+                    <span class="description">{{ discussion.description }}</span>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </v-card-text>
   </v-card>
 </template>
 
 <script>
     export default {
         name: 'ActiveDiscussionCard',
+        props: {
+            discussion: Object
+        },
         data () {
             return {}
         }
@@ -14,4 +34,53 @@
 </script>
 
 <style scoped>
+.card {
+    background-color: #F2F2F2;
+}
+
+.card-text {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.main-class {
+    display: flex;
+    flex-direction: row;
+}
+
+.first-grid {
+    display: flex;
+    flex-direction: column;
+}
+
+.discussion {
+  color: rgba(41, 136, 177, 0.87);
+  font-size: 13px;
+  text-transform: uppercase;
+  font-weight: 500;
+}
+
+.comment-icon {
+  color: #FD953A;
+}
+
+.name {
+    color: #747474;
+    font-size: 36px;
+    font-weight: bold;
+}
+
+.description {
+    margin-top: 8px;
+    font-size: 16px;
+    line-height: 19px;
+    color: #747474;
+}
+
+.photo {
+    border-radius: 100%;
+    width: 20%;
+    height: 20%;
+}
 </style>
--- src/components/CouncilBody.vue
+++ src/components/CouncilBody.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container class="council-body">
+        <ActiveDiscussionCard :discussion="current" />
         <v-row v-for="(card, index) of past" :key="index">
             <v-col class="col-12 d-flex flex-column align-center">
                 <InactiveDiscussionCard :data="card" />
@@ -10,17 +11,26 @@
 
 <script>
     import InactiveDiscussionCard from '@/components/InactiveDiscussionCard'
+    import ActiveDiscussionCard from '@/components/ActiveDiscussionCard'
     import past from '@/assets/mock/past.json';
 
     export default {
         name: 'CouncilBody',
         components: {
-            InactiveDiscussionCard
+            InactiveDiscussionCard,
+            ActiveDiscussionCard
         },
         data () {
             return {
                 active: {},
-                past
+                past,
+                current: {
+                        name: 'Julia',
+                        description: 'Lorem ipsum dolor sit amet',
+                        photo: 'https://randomuser.me/api/portraits/women/81.jpg',
+                        likes: 204,
+                        dislikes: 102
+                }, 
             }
         },
         methods: {
